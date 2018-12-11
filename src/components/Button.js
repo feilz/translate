16c0c22cd3446af4f0b8175f8396df9874cfe135
feilz <ackersdan@gmail.com>
2018-12-11 16:29:39 +0200

    example context

---
+++ src/components/Button.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+class Button extends React.Component {
+    render() {
+        return (
+            <ColorContext.Consumer>
+                {color => (
+                    <button className={`ui button ${color}`}>
+                        <LanguageContext.Consumer>
+                            {value =>
+                                value.language === 'english'
+                                    ? 'Submit'
+                                    : 'Lähetä'
+                            }
+                        </LanguageContext.Consumer>
+                    </button>
+                )}
+            </ColorContext.Consumer>
+        );
+    }
+}
+
+export default Button;
